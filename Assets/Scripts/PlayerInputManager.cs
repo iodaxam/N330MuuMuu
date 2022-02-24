d13086837015bd60c63d7c4a425518da364736bf
Jake <jakegibson0228@gmail.com>
2022-02-24 17:30:16 -0500

    Additional work on multiplayer

---
+++ Assets/Scripts/PlayerInputManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputManager : MonoBehaviour
+{
+	void OnPlayerJoined()
+	{
+		Debug.Log("Player joined");
+	}
+
+}
