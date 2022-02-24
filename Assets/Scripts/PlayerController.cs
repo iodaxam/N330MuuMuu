ab6795a4d764e8f820d9c024dc53f27e79ad1004
WebG1itch <jakegibson0228@gmail.com>
2022-02-24 12:12:29 -0500

    Added support for multiple controllers
    A new player prefab spawns on input from a new controller. If you're using a steam controller, the game has to be added to your steam library to test.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+    private Vector2 movementInput;
+
+    private void Update()
+    {
+        transform.Translate(new Vector3(movementInput.x, 0, movementInput.y) * speed * Time.deltaTime);
+    }
+
+    public void OnMove(InputAction.CallbackContext context) => movementInput = context.ReadValue<Vector2>();
+}
