df1677dd974a9fd290f1aee58a34a115e043d0ca
sBrunes <bruness@att.net>
2022-02-25 16:59:44 -0500

    Updated Combat Script

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
+    private bool CanAttack;
+    private bool Attacking;
 
+    public float AttackDuration = 2f;
+
+    public float CooldownTimerMax = 3f;
+    private float CooldownTimerCurrent;
 
     public void InitiateAttack()
     {
-        Debug.Log("AttackInitiated");
+        if(CanAttack)
+        {
+            Attacking = true;
+            CanAttack = false;
+
+            CooldownTimerCurrent = CooldownTimerMax;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Collided WIth Something");
+        if(other.CompareTag("Player")) {
+            if(Attacking)
+            {
+                Debug.Log("DamageInflicted");
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if(!CanAttack)
+        {
+            CooldownTimerCurrent -= Time.deltaTime;
+
+            if(CooldownTimerCurrent <= (CooldownTimerMax - AttackDuration)) {Attacking = false;}
+
+            if(CooldownTimerCurrent <= 0f) {CanAttack = true;}
+        }
     }
 }
