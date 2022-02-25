f7a20ad267fc6ea9b3f9d23a30383c9a26c2735a
WebG1itch <jakegibson0228@gmail.com>
2022-02-24 20:48:12 -0500

    Camera follows average of players' positions
    Once the map is made, some slight tweaking should be done to make the the camera angles make sense and everything.

--- Assets/Scripts/PlayerInputManager.cs
+++ Assets/Scripts/PlayerInputManager.cs
@@ -1,13 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Object = UnityEngine.Object;
+using Vector3 = UnityEngine.Vector3;
 
 public class PlayerInputManager : MonoBehaviour
 {
+	public GameObject playerAverage;
+	private GameObject[] m_PlayerList;
 	void OnPlayerJoined()
 	{
+		UpdatePlayerList();
 		Debug.Log("Player joined");
+		Debug.Log(m_PlayerList.Length);
 	}
 
+	void OnPlayerLeft()
+	{
+		UpdatePlayerList();
+		Debug.Log("Player Left");
+		Debug.Log(m_PlayerList.Length);
+	}
+
+	void UpdatePlayerList()
+	{
+		m_PlayerList = GameObject.FindGameObjectsWithTag("Player");
+		Debug.Log(m_PlayerList.Length);
+	}
+
+	private void Update()
+	{
+		
+		List<Vector3> playerPositions = new List<Vector3>();
+
+		if (m_PlayerList != null)
+		{
+			foreach (GameObject player in m_PlayerList)
+			{
+				playerPositions.Add(player.transform.position);
+			}
+			playerAverage.transform.position = playerPositions.Aggregate(new Vector3(0,0,0), (s,v) => s + v) / (float)playerPositions.Count;
+		}
+
+	}
 }
