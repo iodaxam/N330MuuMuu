769993c6a8b37358caafeb35bfe2ad963a12de90
WebG1itch <jakegibson0228@gmail.com>
2022-03-05 23:54:51 -0500

    added parent weapon class that others can derive from
    This sets standards for both ranged and melee weapons. I also removed the heavy weapon class and made a melee class that can be used for all melee weapons.

---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Weapons
+{
+    public class Weapon : MonoBehaviour
+    {
+        public Vector3 Position; // prefab tested attachment position
+        public Quaternion Rotation; // prefab tested attachment rotation
+        public Vector3 Scale; // prefab tested attachment scale
+        public GameObject attachmentPoint; // where the weapon is going to attach to on the player prefab.
+        public GameObject weaponPrefab; // prefab of the weapon 
+        public float damage;
+        public float attackTime; // For melee: how long the swing animation is; for ranged: time between shots.
+    
+        protected bool Attacking;
+        protected GameObject weapon;
+        // Start is called before the first frame update
+        void Start()
+        {
+            weapon = Instantiate(weaponPrefab, Vector3.zero, Quaternion.identity); // instantiate a new version of the prefab
+
+            weapon.transform.parent = attachmentPoint.transform;
+            weapon.transform.localPosition = Position;
+            weapon.transform.localRotation = Rotation;
+            weapon.transform.localScale = Scale;
+        }
+    
+        public void Attack() // allow the parent class to tell the weapon it's attacking
+        {
+            Attacking = true;
+            StartCoroutine(nameof(AttackTimer));
+        }
+    
+        private IEnumerator AttackTimer()
+        {
+            yield return new WaitForSeconds(attackTime);
+            Attacking = false;
+            // GetComponentInParent<Animator>().SetBool("Attacking" = false); 
+            // ^ for when we have an animator on the player class ^
+        }
+
+    }
+}
